bf6855ba61c85107b9853f9ba529d9ec33da58ba
user <user@DESKTOP-6OVTNC8>
2025-04-30 18:40:54 +0900

    feat.

--- printLogCheck/src/main/java/com/JangNam/printlog/ExcelExport.java
+++ printLogCheck/src/main/java/com/JangNam/printlog/ExcelExport.java
@@ -1,5 +1,6 @@
 package com.JangNam.printlog;
 
+import java.io.FileOutputStream;
 import java.util.List;
 
 import org.apache.poi.ss.usermodel.*;
@@ -11,20 +12,36 @@ public class ExcelExport {
 		Sheet sheet = workbook.createSheet("출력로그");
 		
 		//머리글
-		String[] headers = {"사용자ID", "이름", "부서", "출력일자", "문서명", "사용자IP"};
+		String[] headers = {"사용자ID", "이름", "부서", "출력일자", "문서명", "프린터이름","사용자IP"};
 		Row headerRows = sheet.createRow(0);
 		for (int i = 0 ; i < headers.length ; i++) {
 			Cell cell = headerRows.createCell(i);
 			cell.setCellValue(headers[i]);
 		}
 		
+		int rowIndex = 1; // 0은 헤더, 1부터 데이터 시작
 		//데이터
 		for (int i = 0 ; i < dataRows.size() ; i++) {
-			Row row = sheet.createRow(i + 1);
-			String[] rowData = dataRows.get(i);
-			for(int j = 0 ; j < rowData.length ; j++) {
-				row.createCell(j).setCellValue(rowData[j]);
-			}
+		    String[] rowData = dataRows.get(i);
+		    if (rowData == null || rowData.length == 0 || rowData[0].trim().isEmpty()) {
+		        continue;
+		    }
+
+		    Row row = sheet.createRow(rowIndex++);
+		    for (int j = 0 ; j < rowData.length ; j++) {
+		        row.createCell(j).setCellValue(rowData[j]);
+		    }
+		}
+		
+		for (int i = 0 ; i < headers.length ; i++) {
+			sheet.autoSizeColumn(i);
+		}
+		
+		try (FileOutputStream fileOut = new FileOutputStream(fileName)){
+			workbook.write(fileOut);
+			System.out.println("엑셀 저장 완료: " + fileName);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		
 		
--- printLogCheck/src/main/java/com/JangNam/printlog/PrintLogParser.java
+++ printLogCheck/src/main/java/com/JangNam/printlog/PrintLogParser.java
@@ -1,32 +1,36 @@
 package com.JangNam.printlog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class PrintLogParser {
-	public static void parse(String html) {
+	public static List<String[]> parse(String html) {
+		List<String[]> result = new ArrayList<>();
+
 		Document doc = Jsoup.parse(html);
 		Elements rows = doc.select("table tbody tr");
-		
-		System.out.printf("%-12s %-6s %-8s %-20s %-30s %-15s\n", "사용자ID", "이름", "부서", "출력일자", "문서명", "사용자IP");
-		
+
+		//System.out.printf("%-12s %-6s %-8s %-20s %-30s %-15s %-15s \n", "사용자ID", "이름", "부서", "출력일자", "문서명", "사용자IP", "프린터");
+
 		for (Element row : rows) {
 			Elements cols = row.select("td");
-			
 			if (cols.size() >= 11) {
-				String userId = cols.get(2).text();
-				String userName = cols.get(3).text();
-				String dept = cols.get(4).text();
-				String printDate= cols.get(6).text();
-				String fileName= cols.get(9).text();
-				String ip = cols.get(11).text();
-				
-				System.out.printf("%-12s %-6s %-8s %-20s %-30s %-15s\n", userId, userName, dept, printDate, fileName, ip);
-				
+				String[] rowData = new String[] { cols.get(2).text(), // 사용자 ID
+						cols.get(3).text(), // 이름
+						cols.get(4).text(), // 부서
+						cols.get(6).text(), // 출력일자
+						cols.get(9).text(), // 문서명
+						cols.get(10).text(), // 프린터이름
+						cols.get(11).text() // 사용자 IP
+				};
+				result.add(rowData);
 			}
 		}
+		return result;
 	}
-
 }
